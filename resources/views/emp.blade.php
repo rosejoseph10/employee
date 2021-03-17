f084bc699640992160b5f92be8dcfb14df02845b
rosejoseph20 <mca542@rajagiri.edu>
2021-03-17 13:20:43 +0400

    msg

---
+++ resources/views/emp.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div class="container">
+<div class="row">
+<div class="col col-12 col-sm-4 col-lg-4 col-xl-4 "></div>
+<table class="table table-borderless">
+<tr>
+    <td>Empname</td>
+    <td><input type="text" class="form-control"></td>
+</tr>
+<tr>
+    <td>Empcode</td>
+    <td><input type="text" class="form-control"></td>
+</tr>
+<tr>
+    <td>Designation</td>
+    <td><input type="text" class="form-control"></td>
+</tr>
+<tr>
+    <td>salary</td>
+    <td><input type="text" class="form-control"></td>
+</tr>
+<tr>
+    <td>DOB</td>
+    <td><input type="text" class="form-control"></td>
+</tr>
+<tr>
+    <td>place</td>
+    <td><input type="text" class="form-control"></td>
+</tr>
+<tr>
+    <td>pincode</td>
+    <td><input type="text" class="form-control"></td>
+</tr>
+<tr>
+    <td><button class="btn btn primary">SUBMIT</button></td>
+
+</tr>
+</div>
+</div>
+</table>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+</body>
+</html>
